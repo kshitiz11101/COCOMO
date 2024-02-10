fbacdc09b84296d36ed1300a6247702453da643d
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-02-10 16:24:14 +0530

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -55,18 +55,18 @@ const Landing = () => {
       stdin: btoa(customInput),
     };
 
-     const options = {
-    method: 'POST',
-    url: process.env.REACT_APP_RAPID_API_URL,
-    params: { base64_encoded: "true", fields: "*" },
-    headers: {
-      "content-type": "application/json",
-      "Content-Type": "application/json",
-      "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    },
-    data: formData,
-  };
+   const options = {
+      method: 'POST',
+      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Host": 'judge0-ce.p.rapidapi.com',
+        "X-RapidAPI-Key": 'd733c648f1mshe43d60590914b45p1a1572jsncc8aa30644b5',
+      },
+      data: formData,
+    };
 
 
     axios.request(options).then(function (response) {
@@ -92,15 +92,16 @@ const Landing = () => {
   };
 
   const CheckStatus = async (token) => {
-    const options = {
-    method: "GET",
-    url: `${process.env.REACT_APP_RAPID_API_URL}/${token}`,
-    params: { base64_encoded: "true", fields: "*" },
-    headers: {
-      "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    },
-  };
+     const options = {
+      method: "GET",
+      url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "X-RapidAPI-Host": 'judge0-ce.p.rapidapi.com',
+        "X-RapidAPI-Key": 'd733c648f1mshe43d60590914b45p1a1572jsncc8aa30644b5',
+      },
+    };
+
 
 
     try {
